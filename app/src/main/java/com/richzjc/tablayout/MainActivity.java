dd10551670977cae52d493a2826c6748e38c18ca
richzjc <zhangjianchuan@wallstreetcn.com>
2018-08-20 22:43:32 +0800

    升级了tablayout

--- app/src/main/java/com/richzjc/tablayout/MainActivity.java
+++ app/src/main/java/com/richzjc/tablayout/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.Button;
 
 import com.richzjc.library.TabLayout;
 
@@ -15,24 +16,81 @@ import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    TabLayout tb;
+    List<String> titles;
+    List<Fragment> fragments;
+    BaseFragmentAdapter fragmentAdapter;
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewPager viewPager = findViewById(R.id.viewpager);
-        TabLayout tb = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewpager);
+        tb = findViewById(R.id.tab);
 
-        final List<Fragment> fragments = new ArrayList <> ();
+        fragments = new ArrayList<>();
         fragments.add(new Fragment());
         fragments.add(new Fragment());
         fragments.add(new Fragment());
-        final List<String> titles = new ArrayList<>();
-        titles.add("android");
-        titles.add("ios");
-        titles.add("backEnd");
-        BaseFragmentAdapter fragmentAdapter = new BaseFragmentAdapter(getSupportFragmentManager());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        fragments.add(new Fragment());
+        titles = new ArrayList<>();
+        titles.add("a");
+        titles.add("b");
+        titles.add("c");
+        titles.add("d");
+        titles.add("e");
+        titles.add("f");
+        titles.add("g");
+        titles.add("h");
+        titles.add("i");
+        titles.add("j");
+        titles.add("k");
+        titles.add("l");
+        titles.add("m");
+        titles.add("n");
+        titles.add("o");
+        fragmentAdapter = new BaseFragmentAdapter(getSupportFragmentManager());
         viewPager.setAdapter(fragmentAdapter);
         fragmentAdapter.configData(titles, fragments);
         tb.setupWithViewPager(viewPager);
+        viewPager.setCurrentItem(8);
+        setListener();
     }
+
+    private void setListener() {
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int index = random.nextInt(titles.size());
+                if (index % 2 == 0) {
+                    titles.remove(index);
+                    fragments.remove(index);
+                    fragmentAdapter.configData(titles, fragments);
+
+                } else {
+                    titles.add(index, System.currentTimeMillis() + "");
+                    fragments.add(index, new Fragment());
+                    fragmentAdapter.configData(titles, fragments);
+                }
+                viewPager.setAdapter(fragmentAdapter);
+                tb.setupWithViewPager(viewPager);
+                viewPager.setCurrentItem(index);
+            }
+        });
+    }
+
 }
